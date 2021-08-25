a1acacabcbf2229dae893a10a479ee8f578253c6
Dylan Foster <dylanfoster@dylan.myfiosgateway.com>
2021-08-24 22:35:33 -0400

    basic chessboard with rules set up, wepback html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-   devtool: 'inline-source-map',
-   devServer: {
-     static: './dist',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Development',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+module.exports = {
+ mode: 'development',
+ entry: {
+   index: './src/index.js',
+ },
+ devtool: 'inline-source-map',
+ devServer: {
+   static: './dist',
+ },
+ plugins: [
+   new HtmlWebpackPlugin({
+     title: 'Development',
+     template: 'src/index.html'
+   }),
+   new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+ ],
+ output: {
+   filename: '[name].bundle.js',
+   path: path.resolve(__dirname, 'dist'),
+   clean: true,
+ },
+};
 
