be647ce536b1342b12a587a3f09da9e40e68438d
Dylan Foster <dylanfoster@dylan.myfiosgateway.com>
2021-08-26 02:01:54 -0400

    remove dead code

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,6 @@ import Chess from "chess.js";
 export const START_FEN =
 	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-export const getFensFromPgn = (chessObj) => {};
-
 export const getMoveName = (index, move) => {
 	if (index % 2 === 0) {
 		return `${Math.floor(index / 2) + 1}. ${move}`;
