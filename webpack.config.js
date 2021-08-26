97d138c3f9f1f5f42644074bbfb3cf6d0c53b39d
Dylan Foster <dylanfoster@dylan.myfiosgateway.com>
2021-08-26 01:31:40 -0400

    add docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
- mode: 'development',
- entry: {
-   index: './src/index.js',
- },
- devtool: 'inline-source-map',
- devServer: {
-   static: './dist',
- },
- module: {
- rules: [
-  {
-    test: /\.pgn/i,
-    use: 'raw-loader',
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./docs",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pgn/i,
+        use: "raw-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "src/index.html",
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
   ],
-},
- plugins: [
-   new HtmlWebpackPlugin({
-     title: 'Development',
-     template: 'src/index.html'
-   }),
-   new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    })
- ],
- output: {
-   filename: '[name].bundle.js',
-   path: path.resolve(__dirname, 'dist'),
-   clean: true,
- },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "docs"),
+    clean: true,
+  },
 };
-
