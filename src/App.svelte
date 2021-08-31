bc4c577dea090c9681b05beb6c8d678fa3065b3e
Dylan Foster <fosterdill@gmail.com>
2021-08-30 23:26:04 -0400

    update theming

--- src/App.svelte
+++ src/App.svelte
@@ -50,23 +50,28 @@
 <main>
 	<button disabled={$tree.currentNode && $tree.currentNode.name !== START_FEN} on:click={handleToggleSide} type="button" class="btn btn-primary">Toggle side</button>
 	<button on:click={handleGoBack} type="button" class="btn btn-primary">Previous move</button>
-	<Board flipped={currentSide === 'black'} />
-	{$engine.adv || '-'}
-	{#if $engine.isWorking}
-		Line: {$engine.line}
-	{:else}
-		Best move: {$engine.bestMove}
-	{/if}
-	{#if $game &&  $tree.currentNode && $tree.currentNode.name === $game.fen()}
-		{#each edges as edge}
-			<div>
-				{edge.name} (won {edge.accum.win} of {edge.accum.total})
-		        <div class="progress" style="width: 200px">
-		          <div class="progress-bar" role="progressbar" style={`width: ${Math.round(100 * edge.accum.win / edge.accum.total)}%`} aria-valuenow={Math.round(100 * edge.accum.win / edge.accum.total)} aria-valuemin="0" aria-valuemax="100"></div>
-		        </div>
-		    </div>
-		{/each}
-	{:else}
-		<div>No positions found</div>
-	{/if}
+	<div style="display: flex">
+		<Board flipped={currentSide === 'black'} />
+		<div>
+			{#if $engine.isWorking}
+		{$engine.adv || '-'}
+				Line: {$engine.line}
+			{:else}
+		{$engine.adv || '-'}
+				Best move: {$engine.bestMove}
+			{/if}
+			{#if $game &&  $tree.currentNode && $tree.currentNode.name === $game.fen()}
+				{#each edges as edge}
+					<div>
+						{edge.name} (won {edge.accum.win} of {edge.accum.total})
+				        <div class="progress" style="width: 200px">
+				          <div class="progress-bar" role="progressbar" style={`width: ${Math.round(100 * edge.accum.win / edge.accum.total)}%`} aria-valuenow={Math.round(100 * edge.accum.win / edge.accum.total)} aria-valuemin="0" aria-valuemax="100"></div>
+				        </div>
+				    </div>
+				{/each}
+			{:else}
+				<div>No positions found</div>
+			{/if}
+		</div>
+	</div>
 </main>
