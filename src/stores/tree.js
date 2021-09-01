a9c1d9c79d4391dd4fc2f9c6052ab10998fa38b6
Dylan Foster <fosterdill@gmail.com>
2021-09-01 18:09:05 -0400

    cleanup

--- src/stores/tree.js
+++ src/stores/tree.js
@@ -14,18 +14,23 @@ const tree = derived(
   [game, nodes],
   ([$game, $nodes]) => {
     const $tree = get(tree);
-    const moveHistory = $game.history().map(getMoveName);
-    const lastMove = moveHistory[moveHistory.length - 1];
-    let newTree = $tree;
 
     // If nodes were just loaded, set the current node to the root node
     if ($nodes && !$tree.currentNode) {
-      newTree.currentNode = $nodes;
+      return {
+        ...$tree,
+        currentNode: $nodes,
+      };
     }
 
+    if (!$tree.currentNode) return $tree;
+
+    const moveHistory = $game.history().map(getMoveName);
+    const lastMove = moveHistory[moveHistory.length - 1];
+
     // If there was a move made that is in the tree, traverse the tree and store the nod
-    if ($nodes && lastMove && lastMove in $tree.currentNode.edges) {
-      newTree = {
+    if (lastMove && lastMove in $tree.currentNode.edges) {
+      return {
         currentNode: $tree.currentNode.edges[lastMove].to,
         previousNodes: $tree.previousNodes.concat([$tree.currentNode]),
       };
@@ -33,16 +38,13 @@ const tree = derived(
 
     // If the game history has fewer moves than the previous nodes, then the user went back a move
     if (moveHistory.length < $tree.previousNodes.length) {
-      newTree = {
+      return {
         currentNode: $tree.previousNodes.pop(),
         previousNodes: $tree.previousNodes,
       };
     }
 
-    return {
-      currentNode: newTree.currentNode,
-      previousNodes: newTree.previousNodes,
-    };
+    return $tree;
   },
   {
     currentNode: null,
